9e19cd13856b654cd840d5aa96c809dc47f0d604
ali <ali8boorboor.dev@gmail.com>
2023-12-25 09:40:45 +0330

    create qr-code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./source/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'my-font':['my-font']
+      },
+    },
+  },
+  plugins: [],
+}
+
